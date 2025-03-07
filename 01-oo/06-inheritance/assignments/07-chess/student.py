339c7dd1b9029858cb3d61665a9bf963119d0cc3
Stephan Buntinx <Stephan.buntinx@student.ucll.be>
2025-03-07 14:14:00 +0100

    exercises

--- 01-oo/06-inheritance/assignments/07-chess/student.py
+++ 01-oo/06-inheritance/assignments/07-chess/student.py
@@ -29,13 +29,11 @@ class Position:
     def __hash__(self):
         return hash((self.x, self.y))
 
-
 class ChessPiece:
-
     def __init__(self, position, color):
-        if not King.is_valid_position(position):
+        if not Pawn.is_valid_position(position):
             raise ValueError('invalid position')
-        if not King.is_valid_color(color):
+        if not Pawn.is_valid_color(color):
             raise ValueError('invalid color')
         self.__position = position
         self.__color = color
@@ -47,7 +45,7 @@ class ChessPiece:
     @property
     def color(self):
         return self.__color
-    
+
     @staticmethod
     def is_valid_color(color):
         return color in ['black', 'white']
@@ -61,26 +59,8 @@ class ChessPiece:
             raise ValueError("illegal move")
         self.__position = new_position
 
+
 class Pawn(ChessPiece):
-    def __init__(self, position, color):
-        if not King.is_valid_position(position):
-            raise ValueError('invalid position')
-        if not King.is_valid_color(color):
-            raise ValueError('invalid color')
-        super().__init__(position,color)
-
-    
-
-  
-
-
-    @staticmethod
-    def is_valid_color(color):
-        return color in ['black', 'white']
-
-    @staticmethod
-    def is_valid_position(position):
-        return 0 <= position.x < 8 and 0 <= position.y < 8
 
     def is_legal_move(self, new_position):
         if not self.is_valid_position(new_position):
@@ -88,37 +68,9 @@ class Pawn(ChessPiece):
         direction = 1 if self.color == 'white' else -1
         return self.position.move(0, direction) == new_position
 
-    def move(self, new_position):
-        if not self.is_legal_move(new_position):
-            raise ValueError("illegal move")
-        self.__position = new_position
-
 
 
 class King(ChessPiece):
-    def __init__(self, position, color):
-        if not King.is_valid_position(position):
-            raise ValueError('invalid position')
-        if not King.is_valid_color(color):
-            raise ValueError('invalid color')
-        self.__position = position
-        self.__color = color
-
-    @property
-    def position(self):
-        return self.__position
-
-    @property
-    def color(self):
-        return self.__color
-
-    @staticmethod
-    def is_valid_color(color):
-        return color in ['black', 'white']
-
-    @staticmethod
-    def is_valid_position(position):
-        return 0 <= position.x < 8 and 0 <= position.y < 8
 
     def is_legal_move(self, new_position):
         if new_position == self.position:
@@ -131,12 +83,5 @@ class King(ChessPiece):
             return False
         return True
 
-    def move(self, new_position):
-        if not self.is_legal_move(new_position):
-            raise ValueError("illegal move")
-        self.__position = new_position
 
-
-# p = Position(4,4)
-# pawn = Pawn(p, "black")
-# print(pawn.color)
+  
---
+++ 01-oo/09-super/assignments/02-shape/student.py
@@ -0,0 +1,50 @@
+from abc import ABC, abstractmethod
+from math import pi
+
+
+class Shape(ABC):
+    @property
+    @abstractmethod
+    def area(self):
+        ...
+
+    @property
+    @abstractmethod
+    def perimeter(self):
+        ...
+
+class Rectangle(Shape):
+
+    def __init__(self, length, width):
+        self.__length = length
+        self.__width = width
+
+    @property
+    def length(self):
+        return self.__length
+    
+    @property
+    def width(self):
+        return self.__width
+    
+class Square(Rectangle):
+    
+    def __init__(self,side):
+        super().__init__(side,side)
+        self.__side = side
+
+    @property
+    def side(self):
+        return self.__side
+    
+class Ellipse:
+
+    def __init__(self, perimeter):
+        self.perimeter = perimeter
+
+    @property
+    def perimeter(self):
+        raise NotImplementedError()
+    
+
+class Circle:
